623df18842fb005481ead86e393205ea0e82863f
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-06-18 13:34:18 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,9 @@
+//comment tag
+/* hello how are you
+this is my first c++ (cpp) project
+This is multiline comment */
+
+#include <iostream>
+int main() {
+    std::cout << "C++ Programming! is fun";
+}
