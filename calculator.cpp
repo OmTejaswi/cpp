b5e7448951a125f028dd05b1237209f3035af309
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-06-18 15:38:59 +0530

    calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include <conio.h>
-void main() {
+int main() {
    float num1;
    float num2;
    char op;
    float result;
 
-   printf("Enter the first number");
+   printf("Enter the first number: ");
+   scanf("%f",&num1);
+   
+   printf("Enter the operation you want to preform (+, -, *, /): ");
+   scanf(" %c",&op);
 
+   printf("Enter the seconf number: ");
+   scanf("%f",&num2);
+
+    switch (op)
+    {
+    case '+':
+    result = num1 + num2;
+    printf("%f",result);
+    break;
+    case '-':
+    result = num1 - num2;
+    printf("%f",result);
+    break;
+    case '*':
+    result = num1 * num2;
+    printf("%f",result);
+    break;
+    case '/':
+    result = num1/num2;
+    printf("%f",result);
+    break;
+    
+    default:
+    printf("The operator is not valid");
+        break;
+    }
 }
