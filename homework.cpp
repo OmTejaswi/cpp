b639fc31dbf5d571aa8eecd299094ffd61d2d73c
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-06-25 13:19:49 +0530

    updated

---
+++ homework.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+#include <typeinfo>
+#include <iostream>
+int main() {
+   float price1;
+   float price2;
+   char op;
+   float result;
+
+   printf("Enter The First Price: ");
+   scanf("%f",&price1);
+//    std::cout << typeid(num1).name();
+   
+   printf("Enter The Operation You Want To Preform ( +, -, *, /: )");
+   scanf(" %c",&op);
+
+   printf("Enter The Second: ");
+   scanf("%f",&price2);
+
+    switch (op)
+    {
+    case '+':
+    result = price1 + price2;
+    printf("%f",result);
+    // printf(typeid(op).name());
+    break;
+    case '-':
+    result = price1 - price2;
+    printf("%f",result);
+    break;
+    case '*':
+    result = price1 * price2;
+    printf("%f",result);
+    break;
+    case '/':
+    result = price1/price2;
+    printf("%f",result);
+    break;
+    
+    default:
+    printf("The operator is not valid");
+        break;
+    }
+}
